e0eafc5cf6c3d08f25b9faf24965533d5a8a33ff
SubramanyaKS <subramanyaks2205@gmail.com>
2025-02-26 22:55:18 +0530

    added homescreen and language util

---
+++ lib/utils/language.dart
@@ -0,0 +1,22 @@
+final Map<String, String> languageMap = {
+  'English': 'en',
+  'Spanish': 'es',
+  'French': 'fr',
+  'German': 'de',
+  'Hindi': 'hi',
+  'Kannada': 'kn',
+  'Chinese (Simplified)': 'zh',
+  'Telugu':'te',
+  'Japanese':'ja',
+};
+
+final Map<String, String> talklanguageMap = {
+  'English': 'en-US',
+  'French': 'fr-FR',
+  'Kannada': 'kn-IN',
+  'Spanish': 'es-ES',
+  'German': 'de-DE',
+  'Hindi': 'hi-IN',
+  'Telugu':'te-IN',
+  'Japanese':'ja-JP',
+};
